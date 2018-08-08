627593c6a27633c93e4ba8ff08fb61d1c53b2cbb
Alen Martinčević <martincevic.alen.development@gmail.com>
2018-08-08 12:49:10 +0200

    Popup control view (only frontend), change password view and functionality

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,4 +1,3 @@
-
 $(function() {
   chrome.storage.sync.get('user', function(data) {
     if (data.user == null) {
@@ -20,19 +19,61 @@ $(document).on('click', '#btnRegister', function(e) {
       'user': user
     });
     $('.container').load('../html/popup_login.html');
-  }else{
-      $("#errText").text("Must not be empty!");
+  } else {
+    $("#errText").text("Must not be empty!");
   }
 });
 
 //Login
 $(document).on('click', '#btnLogin', function(e) {
-  chrome.storage.sync.get('user',function(data){
+  $("#statusText").empty();
+  chrome.storage.sync.get('user', function(data) {
     var enteredPassword = $("#password").val();
-    if(data.user.password == enteredPassword){
+    if (data.user.password == enteredPassword) {
       $('.container').load('../html/popup_control_panel.html');
-    }else{
-      $("#errText").text("Incorrect password");
+    } else {
+      $status = "<div class='alert alert-danger text-center'><strong>Incorrect password</strong></div>";
+      $("#statusText").append($status);
     }
   });
+});
+
+//Change password
+$(document).on('click', "#btnSaveNewPassword", function(e) {
+  $("#statusText").empty();
+  var oldPassword = $("#oldPassword").val();
+  var newPassword = $("#newPassword").val();
+  chrome.storage.sync.get('user', function(data) {
+    if (oldPassword == data.user.password) {
+      if (newPassword != null && newPassword != "") {
+        var user = {
+          password: newPassword
+        };
+        chrome.storage.sync.set({
+          'user': user
+        });
+        $status = "<div class='alert alert-success text-center'><strong>Password changed</strong></div>";
+        $("#statusText").append($status);
+        $("#oldPassword").val("")
+        $("#newPassword").val("");
+      } else {
+        $status = "<div class='alert alert-warning text-center'><strong>New password must not be empty</strong></div>";
+        $("#statusText").append($status);
+      }
+    } else {
+      $status = "<div class='alert alert-danger text-center'><strong>Incorrect old password</strong></div>";
+      $("#statusText").append($status);
+    }
+  });
+});
+
+//Navigate to change password view
+$(document).on('click', '#btnChangePasswordView', function(e) {
+  $('.container').load("../html/popup_change_password.html");
+});
+
+//Navigate to control panel view
+$(document).on('click', '#changePasswordBackLink', function(e) {
+  e.preventDefault();
+  $(".container").load("../html/popup_control_panel.html");
 });
